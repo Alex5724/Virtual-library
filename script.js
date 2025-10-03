e1f45f7a7a74ada7f3af2c9cfc25ce2968dc8a14
Alex <221934805+Alex5724@users.noreply.github.com>
2025-10-03 13:55:54 +0300

    The script fot adding books to library.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const bookTitle = document.querySelector('.title');
 const bookAuthor = document.querySelector('.author');
 const bookPages = document.querySelector('.number-pages');
 const bookRead = document.querySelector('.read');
-const deleteButton = document.querySelector('#delete');
+const library = document.querySelector('.library');
 const addButton = document.querySelector('#addbook');
 
 function Book(title, author, pages, read) {
@@ -24,11 +24,50 @@ function addBookToLibrary() {
     let isRead = "";
 
     isRead = read ? 'Read' : 'Not read';
-
+    if(!title.trim() || !author.trim() || !pages.trim()){
+        alert('Please complete all the empty fields!');
+        return;
+    }
     let book = new Book(title, author, pages, isRead);
     myLibrary.push(book);
-
-  // take params, create a book then store it in the array
 }
 
-addButton.addEventListener('click', addBookToLibrary);
+function revealBooks() {
+  library.innerHTML = '';
+
+  myLibrary.forEach((book, index) => {
+    const newBook = document.createElement('div');
+    newBook.classList.add('book');
+
+    newBook.innerHTML = `
+      <div id="book-head">
+        <div class="book-info">
+          <span class="title">${book.title}</span><br>
+          <span>by <span class="author">${book.author}</span></span>
+        </div>
+        <button class="delete" data-index="${index}">
+          <img src="./icons/trash-can-outline.svg" alt="trashcan icon">
+        </button>
+      </div>
+      <div id="book-body">
+        📖 <span class="number-pages">${book.pages}</span> pages <br>
+        <span class="read">${book.read}</span>
+      </div>
+    `;
+
+    library.appendChild(newBook);
+  });
+}
+
+addButton.addEventListener('click', () => {
+  addBookToLibrary();
+  revealBooks();
+});
+
+library.addEventListener('click', (e) => {
+  if (e.target.closest('.delete')) {
+    const index = e.target.closest('.delete').dataset.index;
+    myLibrary.splice(index, 1); // șterge cartea din array
+    revealBooks(); // reafișează lista
+  }
+});
