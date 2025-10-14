ddb2ef738d23a193b330363aec037a24b3ebaa81
Alex <221934805+Alex5724@users.noreply.github.com>
2025-10-14 13:08:14 +0300

    The script for the book app remaked with class.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+/*const myLibrary = [];
 const bookTitle = document.querySelector('.title');
 const bookAuthor = document.querySelector('.author');
 const bookPages = document.querySelector('.number-pages');
@@ -67,7 +67,83 @@ addButton.addEventListener('click', () => {
 library.addEventListener('click', (e) => {
   if (e.target.closest('.delete')) {
     const index = e.target.closest('.delete').dataset.index;
-    myLibrary.splice(index, 1); // șterge cartea din array
-    revealBooks(); // reafișează lista
+    myLibrary.splice(index, 1); 
+    revealBooks(); 
+  }
+});*/
+
+class Book{
+  constructor(title, author, pages, read){
+    if (!new.target) {
+        throw new Error('Use the keyword new...');
+}
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+  }
+}
+
+class bookApp{
+  #library = [];
+  #libraryContainer = document.querySelector('.library');
+  #addButton = document.querySelector('#addbook');
+  constructor(){
+    this.#addButton.addEventListener('click', () => {
+    this.#addBookToLibrary();
+    this.#revealBooks();
+});
+    this.#libraryContainer.addEventListener('click', (e) => {
+  if (e.target.closest('.delete')) {
+    const index = e.target.closest('.delete').dataset.index;
+    this.#library.splice(index, 1); 
+    this.#revealBooks(); 
   }
 });
+  }
+
+  #addBookToLibrary(){
+    const title = document.querySelector('#title').value.trim();
+    const author = document.querySelector('#author').value.trim();
+    const pages = document.querySelector('#pages').value.trim();
+    const read = document.querySelector('#read-checkbox').checked;
+    let isRead = "";
+
+    isRead = read ? 'Read' : 'Not read';
+    if(!title || !author || !pages){
+        alert('Please complete all the empty fields!');
+        return;
+    }
+    const book = new Book(title, author, pages, isRead);
+    this.#library.push(book);
+  }
+
+  #revealBooks(){
+    this.#libraryContainer.innerHTML = '';
+
+  this.#library.forEach((book, index) => {
+    const newBook = document.createElement('div');
+    newBook.classList.add('book');
+
+    newBook.innerHTML = `
+      <div id="book-head">
+        <div class="book-info">
+          <span class="title">${book.title}</span><br>
+          <span>by <span class="author">${book.author}</span></span>
+        </div>
+        <button class="delete" data-index="${index}">
+          <img src="./icons/trash-can-outline.svg" alt="trashcan icon">
+        </button>
+      </div>
+      <div id="book-body">
+        📖 <span class="number-pages">${book.pages}</span> pages <br>
+        <span class="read">${book.read}</span>
+      </div>
+    `;
+
+    this.#libraryContainer.appendChild(newBook);
+  });
+  }
+}
+
+new bookApp();
